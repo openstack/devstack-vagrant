fc8b4ca1a2269a737b290b69b98279426d733708
Sean Dague <sean@dague.net>
2014-03-25 09:43:06 -0400

    new local.sh

--- puppet/modules/devstack/files/local.sh
+++ puppet/modules/devstack/files/local.sh
@@ -2,17 +2,15 @@
 
 set -o xtrace
 
+echo "Running local.sh"
+
 openrc=/home/stack/devstack/openrc
-source $openrc admin
 
 if is_service_enabled n-api; then
     for user in admin demo; do
         source $openrc $user
         nova keypair-add --pub-key /home/stack/.ssh/authorized_keys default
+        nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+        nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
     done
-
-    source $openrc admin
-
-    nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
-    nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 fi
