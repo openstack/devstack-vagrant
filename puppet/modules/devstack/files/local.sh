aede4ea1a71e5f348b688bdd46582cefb3d1fd26
Sviatoslav Sydorenko <ssydore@softserveinc.com>
2016-07-12 13:12:18 +0300

    Work around invalid public key issue

--- puppet/modules/devstack/files/local.sh
+++ puppet/modules/devstack/files/local.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+echo "Running local.sh"
+
 set -o xtrace
 
 openrc=/home/stack/devstack/openrc
-pubkey_file=/home/stack/.ssh/authorized_keys
-
-echo "Running local.sh"
+authorized_keys_file=/home/stack/.ssh/authorized_keys
 
 source "$openrc"
 
 if is_service_enabled n-api; then
+    # Extract valid public key into tmp file to work around the issue,
+    # introduced by puppet adding comments in the beginning of authorized_keys
+    pubkey_file=`mktemp`
+    grep -vE '^\s*#' "$authorized_keys" | head -n 1 > "$pubkey_file"
+
     for user in admin demo; do
         source "$openrc" "$user" "$user"
         nova keypair-add --pub-key "$pubkey_file" default
         nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
         nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
     done
+
+    # Tmp file cleanup
+    rm -f "$pubkey_file"
 fi
