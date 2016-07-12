d1817670b661f17aaa47e39085dc06fa8fc97eae
Sviatoslav Sydorenko <ssydore@softserveinc.com>
2016-07-12 12:50:35 +0300

    Move public key path into separate variable

--- puppet/modules/devstack/files/local.sh
+++ puppet/modules/devstack/files/local.sh
@@ -2,15 +2,17 @@
 
 set -o xtrace
 
+openrc=/home/stack/devstack/openrc
+pubkey_file=/home/stack/.ssh/authorized_keys
+
 echo "Running local.sh"
 
-openrc=/home/stack/devstack/openrc
-source $openrc
+source "$openrc"
 
 if is_service_enabled n-api; then
     for user in admin demo; do
-        source $openrc $user $user
-        nova keypair-add --pub-key /home/stack/.ssh/authorized_keys default
+        source "$openrc" "$user" "$user"
+        nova keypair-add --pub-key "$pubkey_file" default
         nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
         nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
     done
